779b07ceb643c11942518a34ad129935e2e3fb8a
yng-blood <mfstarboey@gmail.com>
2023-01-23 12:27:52 +0530

    arr of obj

---
+++ array of object/index.js
@@ -0,0 +1,12 @@
+// arrayfucntion = (arr) => {
+let studentRecords = [{ name: 'John', id: 123, marks: 98 },
+    { name: 'Baba', id: 101, marks: 23 },
+    { name: 'yaga', id: 200, marks: 45 },
+    { name: 'Wick', id: 115, marks: 75 }
+];
+
+const str = studentRecords.toUpperCase;
+for (i = 0; i < 4; i++) {
+    console.log(studentRecords[i].name);
+
+}
